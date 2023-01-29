3b43fefb308e72734328dfa4a262e257dd6c9c30
gucio321 <73652197+gucio321@users.noreply.github.com>
2023-01-29 21:12:07 +0100

    animation: remove Start() method - is not needed imao

--- animation.go
+++ animation.go
@@ -2,7 +2,6 @@ package animations
 
 import (
 	"github.com/AllenDang/giu"
-	"time"
 )
 
 // Animation is an interface implemented by each animation.
@@ -11,8 +10,6 @@ type Animation interface {
 	Init()
 	// Reset is called whenever needs to restart animation.
 	Reset()
-	// Start is called along with Animator.Start
-	Start(duration time.Duration, fps int)
 	// Advance is called every frame
 	Advance(procentageDelta float32) (shouldContinue bool)
 	giu.Widget
