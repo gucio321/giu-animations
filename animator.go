6c26be992076bcbdd1efdba595160b4e5b6059a2
gucio321 <gucio321@protonmail.com>
2023-02-15 08:32:48 +0100

    project: move TriggerType and TriggerFunc types into separated file

--- animator.go
+++ animator.go
@@ -16,20 +16,6 @@ const (
 	DefaultDuration = time.Second / 4
 )
 
-type TriggerFunc func() bool
-type TriggerType byte
-
-const (
-	// TriggerNever is a devault value.
-	// Animation will not be started automatically
-	TriggerNever TriggerType = iota
-	// TriggerOnTrue will start animation whenever trigger becomes true
-	TriggerOnTrue
-	// TriggerOnChange will trigger animation, when value of trigger's function
-	// will change
-	TriggerOnChange
-)
-
 // AnimatorWidget is a manager for animation.
 type AnimatorWidget struct {
 	id string
---
+++ trigger.go
@@ -0,0 +1,16 @@
+package animations
+
+type TriggerFunc func() bool
+
+type TriggerType byte
+
+const (
+	// TriggerNever is a devault value.
+	// Animation will not be started automatically
+	TriggerNever TriggerType = iota
+	// TriggerOnTrue will start animation whenever trigger becomes true
+	TriggerOnTrue
+	// TriggerOnChange will trigger animation, when value of trigger's function
+	// will change
+	TriggerOnChange
+)
