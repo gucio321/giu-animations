fb52e1c73f8c7861db5fbd4d9647921375c94290
gucio321 <73652197+gucio321@users.noreply.github.com>
2023-03-11 14:30:30 +0100

    key frame: add mechanism used for obtaining a next/previous KeyFrame

--- key_frame.go
+++ key_frame.go
@@ -1,3 +1,20 @@
 package animations
 
 type KeyFrame byte
+
+func getWithDelta(current KeyFrame, count, delta int) KeyFrame {
+	if delta >= count {
+		panic("multiple-cycles not supported yet (delta >= max")
+	}
+
+	result := int(current) + delta
+	if result < 0 {
+		result = count - result
+	}
+
+	if result >= count {
+		result -= count
+	}
+
+	return KeyFrame(result)
+}
