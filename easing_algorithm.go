680e1205c7544bbe231878fa178aff1312dbc5f9
gucio321 <73652197+gucio321@users.noreply.github.com>
2023-04-02 10:52:15 +0200

    Easing Alg: implement missing easing algorithms
    ref: https://easings.net/

--- easing_algorithm.go
+++ easing_algorithm.go
@@ -13,18 +13,47 @@ type EasingAlgorithmType byte
 
 const (
 	EasingAlgNone EasingAlgorithmType = iota
+
 	EasingAlgInSine
 	EasingAlgOutSine
 	EasingAlgInOutSine
+
+	EasingAlgInQuad
+	EasingAlgOutQuad
+	EasingAlgInOutQuad
+
+	EasingAlgInCubic
+	EasingAlgOutCubic
+	EasingAlgInOutCubic
+
+	EasingAlgInQuart
+	EasingAlgOutQuart
+	EasingAlgInOutQuart
+
+	EasingAlgInQuint
+	EasingAlgOutQuint
+	EasingAlgInOutQuint
+
+	EasingAlgInExpo
+	EasingAlgOutExpo
+	EasingAlgInOutExpo
+
+	EasingAlgInCirc
+	EasingAlgOutCirc
+	EasingAlgInOutCirc
+
 	EasingAlgInBack
 	EasingAlgOutBack
 	EasingAlgInOutBack
+
 	EasingAlgInElastic
 	EasingAlgOutElastic
 	EasingAlgInOutElastic
+
 	EasingAlgInBounce
 	EasingAlgOutBounce
 	EasingAlgInOutBounce
+
 	EasingAlgMax
 )
 
@@ -32,38 +61,71 @@ const (
 // The following condition is expected to be met, however they are not restricted anyhow:
 // 0 <= t <= 1.
 func Ease(alg EasingAlgorithmType, t float32) float32 {
-	switch alg {
-	case EasingAlgNone:
-		return t
-	case EasingAlgInSine:
-		return easingAlgInSine(t)
-	case EasingAlgOutSine:
-		return easingAlgOutSine(t)
-	case EasingAlgInOutSine:
-		return easingAlgInOutSine(t)
-	case EasingAlgInBack:
-		return easingAlgInBack(t)
-	case EasingAlgOutBack:
-		return easingAlgOutBack(t)
-	case EasingAlgInOutBack:
-		return easingAlgInOutBack(t)
-	case EasingAlgInElastic:
-		return easingAlgInElastic(t)
-	case EasingAlgOutElastic:
-		return easingAlgOutElastic(t)
-	case EasingAlgInOutElastic:
-		return easingAlgInOutElastic(t)
-	case EasingAlgInBounce:
-		return easingAlgInBounce(t)
-	case EasingAlgOutBounce:
-		return easingAlgOutBounce(t)
-	case EasingAlgInOutBounce:
-		return easingAlgInOutBounce(t)
+	algs := map[EasingAlgorithmType]EasingAlgorithm{
+		EasingAlgNone: func(t float32) float32 { return t },
+
+		// sine
+		EasingAlgInSine:    easingAlgInSine,
+		EasingAlgOutSine:   easingAlgOutSine,
+		EasingAlgInOutSine: easingAlgInOutSine,
+
+		// quad
+		EasingAlgInQuad:    easingAlgInQuad,
+		EasingAlgOutQuad:   easingAlgOutQuad,
+		EasingAlgInOutQuad: easingAlgInOutQuad,
+
+		// cubic
+		EasingAlgInCubic:    easingAlgInCubic,
+		EasingAlgOutCubic:   easingAlgOutCubic,
+		EasingAlgInOutCubic: easingAlgInOutCubic,
+
+		// quart
+		EasingAlgInQuart:    easingAlgInQuart,
+		EasingAlgOutQuart:   easingAlgOutQuart,
+		EasingAlgInOutQuart: easingAlgInOutQuart,
+
+		// quint
+		EasingAlgInQuint:    easingAlgInQuint,
+		EasingAlgOutQuint:   easingAlgOutQuint,
+		EasingAlgInOutQuint: easingAlgInOutQuint,
+
+		// expo
+		EasingAlgInExpo:    easingAlgInExpo,
+		EasingAlgOutExpo:   easingAlgOutExpo,
+		EasingAlgInOutExpo: easingAlgInOutExpo,
+
+		// circ
+		EasingAlgInCirc:    easingAlgInCirc,
+		EasingAlgOutCirc:   easingAlgOutCirc,
+		EasingAlgInOutCirc: easingAlgInOutCirc,
+
+		// back
+		EasingAlgInBack:    easingAlgInBack,
+		EasingAlgOutBack:   easingAlgOutBack,
+		EasingAlgInOutBack: easingAlgInOutBack,
+
+		// elastic
+		EasingAlgInElastic:    easingAlgInElastic,
+		EasingAlgOutElastic:   easingAlgOutElastic,
+		EasingAlgInOutElastic: easingAlgInOutElastic,
+
+		// bounce
+		EasingAlgInBounce:    easingAlgInBounce,
+		EasingAlgOutBounce:   easingAlgOutBounce,
+		EasingAlgInOutBounce: easingAlgInOutBounce,
 	}
 
-	log.Panicf("Unknown easing type %v", alg)
-	return -1 // unreachable
+	a, found := algs[alg]
+	if !found {
+		log.Panicf("Unknown easing type %v", alg)
+	}
+
+	return a(t)
 }
+
+// === Easing Algorithm Implementations ===
+
+// - Sine:
 
 func easingAlgInSine(plainPercentage float32) float32 {
 	return 1 - float32(math.Cos((float64(plainPercentage)*math.Pi)/2))
@@ -76,6 +138,129 @@ func easingAlgOutSine(p float32) float32 {
 func easingAlgInOutSine(p float32) float32 {
 	return -(float32(math.Cos(math.Pi*float64(p))) - 1) / 2
 }
+
+//- quad
+
+func easingAlgInQuad(p float32) float32 {
+	return p * p
+}
+
+func easingAlgOutQuad(p float32) float32 {
+	return 1 - (1-p)*(1-p)
+}
+
+func easingAlgInOutQuad(p float32) float32 {
+	if p < .5 {
+		return 2 * p * p
+	}
+
+	return float32(1 - math.Pow(float64(-2*p+2), 2)/2)
+}
+
+// - cubic
+
+func easingAlgInCubic(p float32) float32 {
+	return p * p * p
+}
+
+func easingAlgOutCubic(p float32) float32 {
+	return float32(1 - math.Pow(float64(1-p), 3))
+}
+
+func easingAlgInOutCubic(p float32) float32 {
+	if p < .5 {
+		return 4 * p * p * p
+	}
+
+	return float32(1 - math.Pow(float64(-2*p+2), 3)/2)
+}
+
+// - quart
+func easingAlgInQuart(p float32) float32 {
+	return p * p * p * p
+}
+
+func easingAlgOutQuart(p float32) float32 {
+	return float32(1 - math.Pow(float64(1-p), 4))
+}
+
+func easingAlgInOutQuart(p float32) float32 {
+	if p < .5 {
+		return 8 * p * p * p * p
+	}
+
+	return float32(1 - math.Pow(float64(-2*p+2), 4)/2)
+}
+
+// - quint
+
+func easingAlgInQuint(p float32) float32 {
+	return float32(math.Pow(float64(p), 5))
+}
+
+func easingAlgOutQuint(p float32) float32 {
+	return float32(1 - math.Pow(float64(1-p), 5))
+}
+
+func easingAlgInOutQuint(p float32) float32 {
+
+	if p < .5 {
+		return float32(16 * math.Pow(float64(p), 5))
+	}
+
+	return float32(1 - math.Pow(float64(-2*p+2), 5)/2)
+}
+
+// - expo
+
+func easingAlgInExpo(p float32) float32 {
+	if p == 0 {
+		return 0
+	}
+
+	return float32(math.Pow(2, float64(10*p-10)))
+}
+
+func easingAlgOutExpo(p float32) float32 {
+	if p == 0 {
+		return 1
+	}
+
+	return float32(1 - math.Pow(2, float64(-10*p)))
+}
+
+func easingAlgInOutExpo(p float32) float32 {
+	switch {
+	case p == 0:
+		return 0
+	case p == 1:
+		return 1
+	case p < .5:
+		return float32(math.Pow(2, float64(20*p-10)) / 2)
+	default:
+		return float32((2 - math.Pow(2, float64(-20*p+10))) / 2)
+	}
+}
+
+// - circ
+
+func easingAlgInCirc(p float32) float32 {
+	return float32(1 - math.Sqrt(1-math.Pow(float64(p), 2)))
+}
+
+func easingAlgOutCirc(p float32) float32 {
+	return float32(math.Sqrt(1 - math.Pow(float64(p-1), 2)))
+}
+
+func easingAlgInOutCirc(p float32) float32 {
+	if p < .5 {
+		return float32((1 - math.Sqrt(1-math.Pow(float64(2*p), 2))) / 2)
+	}
+
+	return float32((math.Sqrt(1-math.Pow(float64(-2*p+2), 2)) + 1) / 2)
+}
+
+// - back
 
 func easingAlgInBack(p float32) float32 {
 	s := float32(1.70158)
@@ -97,6 +282,8 @@ func easingAlgInOutBack(p float32) float32 {
 	p -= 2
 	return 0.5 * (p*p*((s+1)*p+s) + 2)
 }
+
+// - elastic
 
 func easingAlgInElastic(p float32) float32 {
 	if p == 0 {
@@ -133,6 +320,8 @@ func easingAlgInOutElastic(p float32) float32 {
 		return 0
 	}
 }
+
+// - bounce
 
 func easingAlgInBounce(p float32) float32 {
 	return 1 - easingAlgOutBounce(1-p)
