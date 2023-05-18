cf0832c1447fe56049527d48a9a44d08b9eed056
gucio321 <73652197+gucio321@users.noreply.github.com>
2023-05-18 20:00:31 +0200

    resize: remove state system (not needed anymore\!)

--- resize_animation.go
+++ resize_animation.go
@@ -1,46 +1,9 @@
 package animations
 
 import (
-	"fmt"
 	"github.com/AllenDang/giu"
 	"github.com/AllenDang/imgui-go"
-	"log"
-	"sync"
 )
-
-type resizeAnimationState struct {
-	cursorPos imgui.Vec2
-	m         *sync.Mutex
-}
-
-func (r *resizeAnimationState) Dispose() {
-	// noop
-}
-
-func (r *ResizeAnimation[T]) newState() *resizeAnimationState {
-	return &resizeAnimationState{
-		m: &sync.Mutex{},
-	}
-}
-
-// getState returns animator's state.
-// It could not be public, because of concurrency issues.
-// There is animation bunch of Animator's methods that allows
-// user to obtain certain data.
-func (r *ResizeAnimation[T]) getState() *resizeAnimationState {
-	if s := giu.Context.GetState(r.id); s != nil {
-		state, ok := s.(*resizeAnimationState)
-		if !ok {
-			log.Panicf("error asserting type of animator state: got %T, wanted *animatorState", s)
-		}
-
-		return state
-	}
-
-	giu.Context.SetState(r.id, r.newState())
-
-	return r.getState()
-}
 
 type ResizeAnimation[T giu.Widget] struct {
 	widget resizable2D[T]
@@ -61,52 +24,41 @@ func (r *ResizeAnimation[T]) ID(id string) {
 	r.id = id
 }
 
+// Init implements Animation interface.
+// Call this if you need to re-set animation for widget in a new place.
 func (r *ResizeAnimation[T]) Init() {
-	state := r.getState()
-	state.m.Lock()
-	defer state.m.Unlock()
-	state.cursorPos = imgui.CursorPos()
+	// noop
 }
 
 func (r *ResizeAnimation[T]) Reset() {
-	//r.Init()
+	// noop
 }
 
 func (r *ResizeAnimation[T]) KeyFramesCount() int {
 	return len(r.sizes)
 }
 
-func (r *ResizeAnimation[T]) BuildNormal(currentKeyFrame KeyFrame, starterFunc StarterFunc) {
-	state := r.getState()
-
-	state.m.Lock()
-	imgui.SetCursorPos(
-		imgui.Vec2{
-			X: state.cursorPos.X - (r.sizes[currentKeyFrame].X-r.sizes[0].X)/2,
-			Y: state.cursorPos.Y - (r.sizes[currentKeyFrame].Y-r.sizes[0].Y)/2,
-		})
-	state.m.Unlock()
+func (r *ResizeAnimation[T]) BuildNormal(currentKeyFrame KeyFrame, _ StarterFunc) {
+	c := imgui.CursorPos()
+	imgui.SetCursorPos(imgui.Vec2{
+		X: c.X - (r.sizes[currentKeyFrame].X-r.sizes[0].X)/2,
+		Y: c.Y - (r.sizes[currentKeyFrame].Y-r.sizes[0].Y)/2,
+	})
 
 	r.widget.Size(r.sizes[currentKeyFrame].X, r.sizes[currentKeyFrame].Y).Build()
 }
 
-func (r *ResizeAnimation[T]) BuildAnimation(animationPercentage, arbitraryPercentage float32, baseKeyFrame, destinationKeyFrame KeyFrame, mode PlayMode, starterFunc StarterFunc) {
+func (r *ResizeAnimation[T]) BuildAnimation(animationPercentage, _ float32, baseKeyFrame, destinationKeyFrame KeyFrame, _ PlayMode, _ StarterFunc) {
 	delta := imgui.Vec2{
 		X: (r.sizes[destinationKeyFrame].X - r.sizes[baseKeyFrame].X) * animationPercentage,
 		Y: (r.sizes[destinationKeyFrame].Y - r.sizes[baseKeyFrame].Y) * animationPercentage,
 	}
 
-	state := r.getState()
-	state.m.Lock()
-
-	fmt.Println(delta)
-	imgui.SetCursorPos(
-		imgui.Vec2{
-			X: state.cursorPos.X - (r.sizes[baseKeyFrame].X-r.sizes[0].X+delta.X)/2,
-			Y: state.cursorPos.Y - (r.sizes[baseKeyFrame].Y-r.sizes[0].Y+delta.Y)/2,
-		})
-
-	state.m.Unlock()
+	c := imgui.CursorPos()
+	imgui.SetCursorPos(imgui.Vec2{
+		X: c.X - (r.sizes[baseKeyFrame].X-r.sizes[0].X+delta.X)/2,
+		Y: c.Y - (r.sizes[baseKeyFrame].Y-r.sizes[0].Y+delta.Y)/2,
+	})
 
 	r.widget.Size(r.sizes[baseKeyFrame].X+delta.X, r.sizes[baseKeyFrame].Y+delta.Y).Build()
 }
